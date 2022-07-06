ef7ed7880f059135ec226532369bbd173aeb98d4
Nkechidoris <nkechidoris10@gmail.com>
2022-07-06 16:42:41 -0700

    nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,24 +1,104 @@
 #include "main.h"
 
 /**
- * print_times_table - prints time table
- * @n : times table to use
- * Description: prints the times table
- * Return:void
+ * print - print variable recursively using _putchar
+ * @n: variable to print
  */
 
-void print_times_table(int n)
+void print(int n);
+
+/**
+ * print_to_98 - print all natural numbers from var to 98
+ * @var: the starting limit
+ * Created by: Sanctus-Peter
+ * cc: 5th july, 2022
+ */
+
+void print_to_98(int var)
 {
+	int stopLimit = 98;
 	
-int a = 0, rep, b;
-
-if (n < 0 || n > 15)
-	return;
-
-while (a <= n)
-{
-	for (b = 0; b <= n; b++)
+	if (var > stopLimit)
 	{
-		rep = a * b;
+		int ii;
+		
+		for (ii = var; ii >= stopLimit; ii--)
+		{
+			print(ii);
+			
+			if (ii != stopLimit)
+			{
+
+																	_putchar(',');
+
+																																	_putchar(' ');
+
+																																				}
+
+																						}
+
+											}
+
+				else
+
+						{
+
+									int jj;
+
+
+
+											for (jj = var; jj <= stopLimit; jj++)
+
+														{
+
+																		print(jj);
+
+																					if (jj != stopLimit)
+
+																									{
+
+																														_putchar(',');
+
+																																		_putchar(' ');
+
+																																					}
+
+																							}
+
+												}
+
+					_putchar('\n');
+
+}
+
+
+
+/**
+ *
+ *  * print - to print
+ *
+ *   * @n: var
+ *
+ *    */
+
+void print(int n)
+
+{
+
+		if (n < 0)
+
+				{
+
+							_putchar('-');
+
+									n = -n;
+
+										}
+
+			if (n / 10)
+
+						print(n / 10);
+
+				_putchar(n % 10 + '0');
 
 }
