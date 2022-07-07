412c380ba9207329c6e5e5068309d27f0e634071
Nkechidoris <nkechidoris10@gmail.com>
2022-07-06 20:59:23 -0700

    nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,7 +19,6 @@ void print_times_table(int n)
 				{
 					_putchar(k + '0');
 
-
 				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
@@ -27,9 +26,8 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				
-				} else if (k >= 10 && k < 100)
 
+					} else if (k >= 10 && k < 100)
 					{
 						_putchar(',');
 						_putchar(' ');
@@ -48,5 +46,4 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-
 }
