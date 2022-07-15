34863d02f28dc5d61583640cb65a096981537922
Nkechidoris <nkechidoris10@gmail.com>
2022-07-14 20:59:35 -0700

    pointers arrays strings cntd

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,13 +10,13 @@ char *cap_string(char *s)
 	int i;
 	char sep[] = ",;.!?(){}\n\t\" ";
 	int j;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0)
 		{
 			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] -= 32
+				s[i] -= 32;
 					continue;
 		}
 		for (j = 0; sep[j] != '\0'; j++)
