76a4dbd9466e1c322c1267d09286eb7328f3c69b
Nkechidoris <nkechidoris10@gmail.com>
2022-07-14 20:39:07 -0700

    pointers arrays strings cntd

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers.
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int k;
+	
+	i = 0;
+	while (i < n / 2)
+	{
+		k = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = k;
+		i++;
+	}
+}
